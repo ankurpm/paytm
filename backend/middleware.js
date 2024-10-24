e9ebec691a3c1d116962bd8c0570bbb7306d14a8
ankurpm <37891760+ankurpm@users.noreply.github.com>
2024-10-24 19:32:02 +0530

    BE: add auth middleware

---
+++ backend/middleware.js
@@ -0,0 +1,19 @@
+const jwt = require("jsonwebtoken");
+const { JWT_SECRET } = require("./config");
+
+function authMiddleware(req, res, next) {
+    const bearerToken = req.header('Authorization')
+    const token = bearerToken.split(' ')[1]
+    try {
+        const decodedToken = jwt.verify(token, JWT_SECRET);
+        req.userId = decodedToken.username;
+    } catch (err) {
+        return res.status(403).json(
+            { msg: "Invalid token" }
+        );
+    }
+
+    next()
+}
+
+module.exports = authMiddleware
